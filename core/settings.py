1cbe7ebba9b93a8e7ddde5a1f6d5e35cc61d4c20
princewill-dev <ezekudep@gmail.com>
2024-03-20 22:20:06 +0100

    Update .gitignore and core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,17 +15,20 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import environ
 import os
-from dotenv import load_dotenv
-load_dotenv()
+
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
